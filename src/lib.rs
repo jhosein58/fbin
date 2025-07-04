222c18c9398c501cf91e104098c7303ec2979bbf
jhosein58 <jhosein58@gmail.com>
2025-07-05 02:08:58 +0330

    remove and other changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,46 @@
-use std::fs::{OpenOptions, File};
+use std::fs::{OpenOptions, File, remove_file as rm_file};
 use std::io::{Error, Seek, SeekFrom, Write};
 
 
 pub struct FBin {
+    path: String,
     handle: File,
     offset: u64,
 }
 
 impl FBin {
 
-    fn def_option_generator() -> OpenOptions{
-        OpenOptions::new().read(true).write(true).to_owned()
+    fn get_handle(path: &str, read: bool, write: bool, create: bool, new: bool) -> Result<File, Error>{
+        OpenOptions::new().read(read).write(write).create(create).create_new(new).open(path)
+    }
+
+    fn get_instance(path: &str, read: bool, write: bool, create: bool, new: bool) -> Result<Self, Error> {
+        Self::get_handle(path,  read, write, create, new)
+        .map(|file| Self {path: path.to_string(), handle: file, offset: 0 })
     }
 
     pub fn open(path: &str) -> Result<Self, Error> { 
-        Ok(Self { handle:  Self::def_option_generator().open(path)?, offset: 0 })
+        Self::get_instance(path, true, true, false, false)
     }
+
     pub fn open_or_create(path: &str) -> Result<Self, Error> { 
-        Ok(Self { handle:   Self::def_option_generator().create(true).open(path)?, offset: 0 })
+        Self::get_instance(path, true, true, true, false)
     }
+
     pub fn create(path: &str) -> Result<Self, Error> { 
-        Ok(Self { handle:   Self::def_option_generator().create_new(true).open(path)?, offset: 0 })
+        Self::get_instance(path, true, true, false, true)
+    }
+
+    pub fn file_exists(path: &str) -> bool {
+        Self::open(path).is_ok()
+    }
+
+    pub fn remove(self) -> Result<(), Error>{
+        rm_file(self.path)
+    }
+    
+    pub fn remove_file(path: &str) -> Result<(), Error>{
+        rm_file(path)
     }
 
     pub fn seek(&mut self, to: u64) -> Result<&mut Self, Error>{
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use fbin::FBin;
 
 fn main() -> Result<(), Error> {
 
+    println!("{:?}", FBin::file_exists("test"));
     FBin::open_or_create("test")?.seek(10)?.write(b"Hello World")?;
+    println!("{:?}", FBin::file_exists("test"));
 
     Ok(())
 
